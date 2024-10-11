e3e769575dce198dabac656343246857b8767864
Sivaani <sivaanijanaswamy@gmail.com>
2024-10-11 23:39:02 +0530

    new file

---
+++ Basic_Info.py
@@ -0,0 +1,5 @@
+#variable declaration and intitalisation 
+a = 5
+#python identation is very important to differentiate between different parts of the program
+#how to input a string 
+s = input("Enter a string = ")
