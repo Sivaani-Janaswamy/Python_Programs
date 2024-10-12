111b56eb5e8838901614fb1590c90f61b30ce455
Sivaani <sivaanijanaswamy@gmail.com>
2024-10-12 11:16:34 +0530

    new file

---
+++ EvenOdd.py
@@ -0,0 +1,12 @@
+"""
+Write a Python Program to count no of even numbers and odd numbers from the given list
+Give distict numbers as input values
+Sample Input:
+5
+12345
+Description:The first line of input consists of a single integer N denoting the total number of input
+The second line of input consists os Nspace separated integers, denoting the individual elements in the input
+Sample output:
+23
+Output description:Output a single line with ocunt of even numbers and odd numbers from N, separated by a single space. 
+"""
