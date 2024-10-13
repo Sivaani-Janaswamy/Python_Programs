a43a9bf54049e243a7876a935ef4caade150602f
Sivaani <sivaanijanaswamy@gmail.com>
2024-10-13 09:32:57 +0530

    New file

--- EvenOdd.py
+++ EvenOdd.py
@@ -5,8 +5,17 @@ Sample Input:
 5
 12345
 Description:The first line of input consists of a single integer N denoting the total number of input
-The second line of input consists os Nspace separated integers, denoting the individual elements in the input
+The second line of input consists of Nspace separated integers, denoting the individual elements in the input
 Sample output:
 23
 Output description:Output a single line with ocunt of even numbers and odd numbers from N, separated by a single space. 
 """
+n = int(input())
+l = [int(i) for i in input().split]
+e,o = 0,0
+for i in l:
+   if(i%2==0):
+    e+=1
+   else:
+    o+=1
+print(e," ",o)
